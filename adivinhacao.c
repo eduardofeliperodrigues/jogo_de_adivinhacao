4c2604a09515326dbec21415c916958b63abf167
Eduardo Felipe Rodrigues <contatoeduardofelipe@hotmail.com>
2020-03-23 22:00:39 -0300

    Laço de vidas criado para contabilizar vidas

--- adivinhacao.c
+++ adivinhacao.c
@@ -7,16 +7,49 @@ int main()
     printf("* Bem vindo ao programa de advinhacao *\n");
     printf("***************************************\n");
     
+    //Geração do numero aleatório
     int random_number = 42;
     
+    //Valor do chute
     int chute;
-    
-    // Pede para o usuario digitar o seu chute;
-    printf("Qual eh o seu chute? ");
-    scanf("%d", &chute);
-    printf("\nSeu chute foi %d\n", chute);
-    
-    
-    
+        
+    // Numero de vidas
+    int lives = 1;
+        
+    while (lives < 4){
+        
+        printf("\n\n***************************************\n");
+        
+        //Imprime a vida atual
+        printf("\n\nTentativa %d\n", lives);
+        
+        // Pede para o usuario digitar o seu chute;
+        printf("\nQual eh o seu chute? ");
+        scanf("%d", &chute);
+        printf("\nSeu chute foi %d\n", chute);
+        
+        // Comparação do chute com o numero aleatório
+        
+        int acertou = (chute == random_number);
+        int maior = chute > random_number;
+
+        if (acertou){
+
+            printf("\nVoce acertou, parabens!\n");
+            break;
+        }
+        else{
+            if (maior){
+                printf("\nSeu chute foi maior que o numero secreto.\n");
+            }
+            else{
+                printf("\nSeu chute foi menor que o numero secreto.\n");
+            }
+        }
+        
+        
+        lives ++;
+    }
+    printf("\n\n***** Suas vidas acabaram *****");   
     return 0;
 }
