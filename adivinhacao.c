45b620ba5cdaad5f4812edd845c6b79f32d73b50
Eduardo Felipe Rodrigues <contatoeduardofelipe@hotmail.com>
2020-03-24 18:04:48 -0300

    Remoção de pontuação, colocação definida por numero de chutes

--- adivinhacao.c
+++ adivinhacao.c
@@ -9,36 +9,31 @@ int main()
     printf("***************************************\n");
     
     //Geração do numero aleatório
-    int random_number = 42;
+    float random_number = 42;
     
     //Valor do chute
-    int chute;
+    float chute;
         
     // Numero de vidas
-    int lives = 1;
-    
-    // Valor da pontuação inicial de cada jogador;
-    int pontos = 1000;
-    
-    int pontos_perdidos;
+    float lives = 1;
         
     while(1){
         
         printf("\n\n***************************************\n");
         
         //Imprime a vida atual
-        printf("\n\nTentativa numero %d\n", lives);
+        printf("\n\nTentativa numero %4.0f\n", lives);
         
         // Pede para o usuario digitar o seu chute;
         printf("\nQual eh o seu chute? ");
-        scanf("%d", &chute);
-        printf("\nSeu chute foi %d\n", chute);
+        scanf("%f", &chute);
+        printf("\nSeu chute foi %.0f\n", chute);
         
         while (chute < 0){
             // Pede para o usuario digitar o seu chute;
             printf("\nVoce digitou um valor menor que 0, digite novamente: ");
-            scanf("%d", &chute);
-            printf("\nSeu chute foi %d\n", chute);
+            scanf("%f", &chute);
+            printf("\nSeu chute foi %.0f\n", chute);
         }
         
         // Comparação do chute com o numero aleatório
@@ -48,25 +43,20 @@ int main()
 
         if (acertou){
             system("cls");
-            printf("*****************************************\n");
-            printf("\n*       Voce acertou, parabens!         *\n");
-            printf("\n*  Voce acertou o jogo em %d tentativas  *\n", lives);
-            printf("\n*   Sua pontuacao final eh %d pontos!  *\n\n", pontos);
-            printf("*****************************************\n");
+            printf("********************************************\n");
+            printf("\n*         Voce acertou, parabens!          *\n");
+            printf("\n*  Voce acertou o jogo em %4.0f tentativas  *\n", lives);
+            printf("\n********************************************\n");
             break;
         }
         else if (maior){
             printf("\nSeu chute foi maior que o numero secreto.\n");
-            pontos_perdidos = (chute - random_number)/2;
         }
         else {
             printf("\nSeu chute foi menor que o numero secreto.\n");
-            pontos_perdidos = (random_number - chute)/2;
         }
         
         lives ++;
-        
-        pontos = pontos - pontos_perdidos;
     }
     
     return 0;
